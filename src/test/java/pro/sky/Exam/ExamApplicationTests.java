3ce58c936936073ad0a5395e31957dc411571d58
rahhiore <hareharrionh@gmail.com>
2023-08-25 06:37:05 +0300

    first commit

---
+++ src/test/java/pro/sky/Exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
